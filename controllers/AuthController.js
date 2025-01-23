eb9c079e0e6b5ae5bd809ef52dc5aa4bc028265b
ngari-kev <kevngariwangui@gmail.com>
2025-01-23 14:31:54 +0300

    Authenticate a user

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,7 +3,6 @@ import sha1 from 'sha1';
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-
 export default class AuthController {
   static async getConnect(req, res) {
     // Get Authorization header
